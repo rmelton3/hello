fcf0728bf43b4d44b24d72ec2fada8b261c72ad3
rmelton3 <123668403+rmelton3@users.noreply.github.com>
2023-08-24 17:10:20 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,19 @@
+/**
+ * Author: Riley Melton
+ *         rmelton3@huskers.unl.edu
+ * Date: 2023/08/22
+ *
+ * Hack 1
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+
+  printf("Hello world, I'm Riley!\n");
+  printf("I am an Electrical Engineer major!\n");
+
+  return 0;
+}
